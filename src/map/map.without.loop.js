b6a71b159124bc811e6c04fd706002c1db1fbfe5
Santosh Marigowda <santosh.marigowda@abcam.com>
2022-01-23 11:32:23 +0000

    another version of map with concat

--- src/map/map.without.loop.js
+++ src/map/map.without.loop.js
@@ -3,6 +3,15 @@ function map(fn, array) {
   return [fn(array[0])].concat(map(fn, array.slice(1)));
 }
 
+function concat(array1, array2) {
+  return array1.concat(array2);
+}
+
+function mapv2(fn, array) {
+  if (array.length === 0) return [];
+  return concat([fn(array[0])], mapv2(fn, array.slice(1)));
+}
+
 let myArray = [1, 2, 3, 4, 5];
 
 let result1 = map((n) => n * 2, myArray); //?
@@ -16,3 +25,7 @@ const rec4 = [2].concat([4, 6, 8, 10]); //?
 const rec5 = [2, 4, 6, 8, 10]; //?
 
 let result2 = map((n) => n / 2, myArray); //?
+
+//================
+
+let result3 = mapv2(n => n * 5, myArray) //?
